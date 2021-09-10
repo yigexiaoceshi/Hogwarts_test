69abd3149b75eb9b7d4dcae125c026a4be46fd1f
yigexiaoceshi <1337799520@qq.com>
2021-09-10 14:54:32 +0800

    提交第二次作业：web自动化（企业微信实现自动添加联系人），更改get_members()里的members变量名为phone_list

--- homework/homework_PO_webauto_test/PageObject_company_wechat/page_contact.py
+++ homework/homework_PO_webauto_test/PageObject_company_wechat/page_contact.py
@@ -48,21 +48,21 @@ class PageContact(PageBase):
             # buttom_left = self.br.execute_script("return document.querySelector('.js_pre_page')")
             buttom_right = self.br.execute_script("return document.querySelector('.js_next_page')")
             if i == 1:
-                # 当页码为1时，获取当前页数据，将手机号码写入members
+                # 当页码为1时，获取当前页数据，将手机号码写入phone_list
                 member_start = self.br.find_elements(By.CSS_SELECTOR,
                                                      '#member_list>.member_colRight_memberTable_tr>.member_colRight_memberTable_td:nth-child(5)')
                 print(member_start)
                 for member in member_start:
                     phone_list.append(member.text)
             else:
-                # 当页码不为1，且下一页按钮可点击时，点击【下一页】按钮，再获取当前页数据，手机号码依次写入members
+                # 当页码不为1，且下一页按钮可点击时，点击【下一页】按钮，再获取当前页数据，手机号码依次写入phone_list
                 if buttom_right.is_enabled():
                     self.br.find_element(By.CSS_SELECTOR, '.js_next_page').click()
                     sleep(2)
-                    member_middle = self.br.find_elements(By.CSS_SELECTOR,
+                    member_middle_end = self.br.find_elements(By.CSS_SELECTOR,
                                                           '#member_list>.member_colRight_memberTable_tr>.member_colRight_memberTable_td:nth-child(5)')
-                    print(member_middle)
-                    for member in member_middle:
+                    print(member_middle_end)
+                    for member in member_middle_end:
                         phone_list.append(member.text)
                 else:
                     pass
@@ -75,9 +75,9 @@ class PageContact(PageBase):
         return phone_list
         # 思路2：
         # 1、定义上一页按钮，下一页按钮，使用if_elif_else判断语句
-        # 2、上一页按钮不可点击时，获取第一页数据，提取text放到members
-        # 3、上一页和下一页按钮均可点击时，获取中间页面数据，提取text放到members
-        # 4、下一页按钮不可点击时，获取最后一页数据，提取text放到members
+        # 2、上一页按钮不可点击时，获取第一页数据，提取text放到phone_list
+        # 3、上一页和下一页按钮均可点击时，获取中间页面数据，提取text放到phone_list
+        # 4、下一页按钮不可点击时，获取最后一页数据，提取text放到phone_list
 
     # 通讯录页面，方法3：删除成员
     def delete_member(self, phone_number):
