f7a87bd9a1a2f81b26556be5cdb4954f3f16d75a
yigexiaoceshi <1337799520@qq.com>
2021-09-25 12:35:17 +0800

    Python基础巩固

--- Python基础巩固/part_008_集合/004_综合练习.py
+++ Python基础巩固/part_008_集合/004_综合练习.py
@@ -22,8 +22,8 @@ library = [{"book_name": "红楼梦", "author": "曹雪芹", "price": 100, "numb
 print('~~~~~~~~~~~~~~~~~~欢迎来到图书管理系统~~~~~~~~~~~~~~~~~~')
 flag = True
 while flag:
-    choice = int(input("请选择功能：\n1.借书\n2.还书\n3.查询书\n4.查询所有库存\n5.退出系统\n"))
-    if choice == 1:
+    choice = input("请选择功能：\n1.借书\n2.还书\n3.查询书\n4.查询所有库存\n5.退出系统\n")
+    if choice == "1":
         print("欢迎使用借书功能！")
         bbbb = True
         while bbbb:
@@ -43,42 +43,46 @@ while flag:
                 print("符合书名的书籍信息为：", borrow_books)
                 borrow_author = input("请输入书籍的作者：")
                 success_book = []
+                borrow_authors = []
                 for aa in borrow_books:
                     if aa["author"] == borrow_author:
-                        print("名字为{}的书中找到了作者为{}的书。".format(borrow_book_name, borrow_author))
-                        for e, f in aa.items():
-                            print(str(e).ljust(10), str(f).ljust(10), end="")
-                        success_book.append(aa)
-                        print()
-                        aaaa = True
-                        while aaaa:
-                            borrow_number = input("请输入要借的数量：")
-                            for bb in success_book:
-                                if borrow_number.isdigit():
-                                    if int(borrow_number) <= bb["number"]:
-                                        print("借阅数量不大于库存数量，支持借阅")
-                                        answer = input("是否借阅？(输入y或Y确定，其他任意键退出！)")
-                                        if answer.upper() == "Y":
-                                            print("借书成功！")
-                                            for cc in library:
-                                                if cc["book_name"] == borrow_book_name and cc["author"] == borrow_author:
-                                                    cc["number"] -= int(borrow_number)
-                                            aaaa = False
-                                            bbbb = False
+                        borrow_authors.append(aa["author"])
+                        if borrow_author in borrow_authors:
+                            print("名字为{}的书中找到了作者为{}的书。".format(borrow_book_name, borrow_author))
+                            for e, f in aa.items():
+                                print(str(e).ljust(10), str(f).ljust(10), end="")
+                            success_book.append(aa)
+                            print()
+                            aaaa = True
+                            while aaaa:
+                                borrow_number = input("请输入要借的数量：")
+                                for bb in success_book:
+                                    if borrow_number.isdigit():
+                                        if int(borrow_number) <= bb["number"]:
+                                            print("借阅数量不大于库存数量，支持借阅")
+                                            answer = input("是否借阅？(输入y或Y确定，其他任意键退出！)")
+                                            if answer.upper() == "Y":
+                                                print("借书成功！")
+                                                for cc in library:
+                                                    if cc["book_name"] == borrow_book_name and cc["author"] == borrow_author:
+                                                        cc["number"] -= int(borrow_number)
+                                                aaaa = False
+                                                bbbb = False
+                                            else:
+                                                aaaa = False
+                                                bbbb = False
                                         else:
-                                            aaaa = False
-                                            bbbb = False
-                                else:
-                                    print("数量输入有误，请重新输入")
-                            else:
-                                print("库存不足，请重新输入")
-                        break
+                                            print("库存不足，请重新输入")
+                                    else:
+                                        print("数量输入有误，请重新输入")
+                            break
+                        else:
+                            print("名字为'{}'的书中未找到作者为'{}'的书。请重新输入！".format(borrow_book_name, borrow_author))
                     else:
-                        print("名字为'{}'的书中未找到作者为'{}'的书。请重新输入！".format(borrow_book_name, borrow_author))
-
+                        pass
             else:
                 print("您输入的书名不在图书馆库存中，请重新输入！")
-    elif choice == 2:
+    elif choice == "2":
         return_book_name = input("请输入要归还的书名：")
         return_book_author = input("请输入要归还的书籍的作者：")
         return_book_number = input("请输入要归还的数量：")
@@ -92,7 +96,7 @@ while flag:
             else:
                 print("您选择了不再还书，即将退出还书功能！")
                 break
-    elif choice == 3:
+    elif choice == "3":
         print("欢迎使用图书查询功能！")
         while True:
             find = input("请输入需要查找的书名(book_name)或作者(author):")
@@ -131,14 +135,16 @@ while flag:
                     pass
                 else:
                     break
-    elif choice == 4:
+    elif choice == "4":
         print("欢迎使用查询库存功能，实时库存如下：")
         for i in library:
             for k, v in i.items():
                 print(str(k).ljust(10), str(v).ljust(10), end="")
             print()
-    elif choice == 5:
+    elif choice == "5":
         print("正在退出图书管理系统，请稍后...")
         sleep(1)
         print("退出成功，欢迎再次使用！")
         flag = False
+    else:
+        print("输入有误，请重新选择！")
