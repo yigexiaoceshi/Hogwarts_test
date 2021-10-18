f351b7d013efa0cda4458bfa077cfe5c82c7b712
yigexiaoceshi <1337799520@qq.com>
2021-10-18 14:30:04 +0800

    Python基础巩固

--- Python基础巩固/part_009_函数/015_装饰器带参数.py
+++ Python基础巩固/part_009_函数/015_装饰器带参数.py
@@ -1,7 +1,7 @@
 # !/usr/bin/python3
 # -*- coding:utf-8 -*-
 """
-装饰器带参数
+装饰器带参数：多定义一层
 """
 
 
@@ -24,3 +24,41 @@ def play_game():
 
 
 print(play_game())
+
+"""
+装饰器应用场景
+"""
+is_login = False
+
+
+def login():
+    username = input("请输入登录用户名：")
+    password = input("请输入登录密码：")
+    if username == "admin" and password == "1234":
+        return True
+    else:
+        return False
+
+
+def login_required(func):
+    def wrapper(*args, **kwargs):
+        global is_login
+        if is_login:  # 引用全局变量，无需global
+            func(*args, **kwargs)
+        else:
+            print("请先登录")
+            is_login = login()  # 改变全局变量的值，必须global
+            print("登录结果为：", is_login)
+
+    return wrapper
+
+
+@login_required
+def pay(money):
+    print("---开始付款---")
+    print("---正在付款---")
+    print(f"---成功付款{money}元---")
+
+
+pay(999)
+pay(888)
