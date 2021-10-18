4b371965a808f653a2bab1db9ed88bcd1fa6982a
yigexiaoceshi <1337799520@qq.com>
2021-10-18 14:01:06 +0800

    Python基础巩固

--- Python基础巩固/part_009_函数/015_装饰器带参数.py
+++ Python基础巩固/part_009_函数/015_装饰器带参数.py
@@ -5,17 +5,17 @@
 """
 
 
-def outer_check(time):
-    def check_time(action):
-        def do_action():
+def outer_check(time):  # 第一层：接收装饰器的参数
+    def check_time(action):  # 第二层：接收原函数
+        def do_action():  # 第三层：接收原函数的参数
             if time < 22:
                 return action()
             else:
                 return "对不起，您不具备该权限！"
 
-        return do_action
+        return do_action  # 返回第三层
 
-    return check_time  # 此处返回check_time之后，为什么会执行check_time()，语句先后顺序该函数已经提前加载到内存了
+    return check_time  # 返回第二层
 
 
 @outer_check(21)
