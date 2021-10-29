e054eaa5de1db0b4a5b991f6cd7daf5e84bca9c4
yigexiaoceshi <1337799520@qq.com>
2021-10-29 10:52:08 +0800

    Python基础巩固

--- homework/homework_PO_webauto_test/PageObject_company_wechat/page_contact.py
+++ homework/homework_PO_webauto_test/PageObject_company_wechat/page_contact.py
@@ -48,14 +48,14 @@ class PageContact(PageBase):
             # buttom_left = self.br.execute_script("return document.querySelector('.js_pre_page')")
             buttom_right = self.br.execute_script("return document.querySelector('.js_next_page')")
             if i == 1:
-                # 当页码为1时，获取当前页数据，将手机号码写入phone_list
+                # 获取首页数据，将手机号码写入phone_list
                 member_start = self.br.find_elements(By.CSS_SELECTOR,
                                                      '#member_list>.member_colRight_memberTable_tr>.member_colRight_memberTable_td:nth-child(5)')
                 print(member_start)
                 for member in member_start:
                     phone_list.append(member.text)
             else:
-                # 当页码不为1，且下一页按钮可点击时，点击【下一页】按钮，再获取当前页数据，手机号码依次写入phone_list
+                # 当下一页按钮可点击时，点击【下一页】按钮，再获取当前页数据，手机号码依次写入phone_list
                 if buttom_right.is_enabled():
                     self.br.find_element(By.CSS_SELECTOR, '.js_next_page').click()
                     sleep(2)
