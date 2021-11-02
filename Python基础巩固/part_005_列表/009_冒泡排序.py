8ada60c6c5d19e037f77b56104278848e504fa1e
yigexiaoceshi <1337799520@qq.com>
2021-11-02 13:51:40 +0800

    Python基础巩固

--- Python基础巩固/part_005_列表/009_冒泡排序.py
+++ Python基础巩固/part_005_列表/009_冒泡排序.py
@@ -2,9 +2,12 @@
 # -*- coding:utf-8 -*-
 import random
 
+print("*" * 20, "生成列表，元素为8位1-100内的随机数字", "*" * 20)
 list = []
 for aaa in range(8):
-    list.append(random.randint(1,100))
+    list.append(random.randint(1, 100))
+print(list)
+
 print("*" * 20, "遍历列表：while循环", "*" * 20)
 n = 0
 while n < len(list):
@@ -25,7 +28,7 @@ print("\n", "*" * 20, "例题1：冒泡排序，从小到大排序", "*" * 20)
 # 随机生成8位1-100内的整数，放入列表list1
 list1 = []
 for aaa in range(8):
-    list1.append(random.randint(1,100))
+    list1.append(random.randint(1, 100))
 # 冒泡排序，升序排列
 for i in range(len(list1) - 1):
     for j in range(len(list1) - 1 - i):
@@ -39,7 +42,7 @@ print("\n", "*" * 20, "例题1：冒泡排序，从大到小排序", "*" * 20)
 # 随机生成8位1-100内的整数，放入列表list2
 list2 = []
 for aaa in range(8):
-    list2.append(random.randint(1,100))
+    list2.append(random.randint(1, 100))
 # 冒泡排序，降序排列
 for h in range(len(list2) - 1):
     for k in range(len(list2) - h - 1):
