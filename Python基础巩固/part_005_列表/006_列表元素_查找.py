2f47f1ef2f31a0f135c375d78c143e6c5539e248
yigexiaoceshi <1337799520@qq.com>
2021-11-02 10:52:19 +0800

    Python基础巩固

--- Python基础巩固/part_005_列表/006_列表元素_查找.py
+++ Python基础巩固/part_005_列表/006_列表元素_查找.py
@@ -10,8 +10,8 @@ else:
 
 
 print("*" * 20, "查找列表中元素的索引：list.index('元素名')，返回索引值，元素不存在则会报错，一般结合if条件使用，避免报错", "*" * 20)
-if "老虎" in list1:
-    print(list1.index("老虎"))
+if "老虎" in list1:  # 先判断元素是否存在
+    print(list1.index("老虎"))  # 存在再查找索引
     print("元素老虎的索引值是：{}".format(list1.index("老虎")))
 else:
     print("元素'老虎'不在列表list1里面！")
